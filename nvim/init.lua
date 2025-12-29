2458df966bee8eb785b9c7a03e1a1d5a35e2d693
spencer <spencerglendenning@gmail.com>
2025-12-29 15:27:16 +1300

    init

--- nvim/init.lua
+++ nvim/init.lua
@@ -100,5 +100,17 @@ config.setup({
 
 vim.opt.number = true
 
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
+
 
 
