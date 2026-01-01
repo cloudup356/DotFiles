ff1f0d72919ff57b6de541f226c0ba05346a2585
spencer <spencerglendenning@gmail.com>
2026-01-01 19:06:14 +1300

    init

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,6 +27,11 @@ require("lazy").setup({
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
 	{
+ 	 'nvim-lualine/lualine.nvim',
+  	dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+
+	{
     	'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
      	dependencies = { 'nvim-lua/plenary.nvim' }
     	},
@@ -112,4 +117,21 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 
+
+require('lualine').setup {
+  options = {
+    theme = 'auto', 
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    globalstatus = true, 
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+}
 
