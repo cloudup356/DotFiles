b10d77c87f2a917796b404aa8c6bd3301a3be5b5
spencer <spencerglendenning@gmail.com>
2026-04-10 13:32:14 +1200

    updtae

--- nvim-config-2/lua/user/plugins/lsp.lua
+++ nvim-config-2/lua/user/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
     dependencies = {
+      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       require("mason").setup()
@@ -13,6 +14,24 @@ return {
         },
         automatic_installation = true,
       })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      
+      vim.lsp.config('clangd', {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config('lua_ls', { capabilities = capabilities })
+      vim.lsp.config('pyright', { capabilities = capabilities })
+      vim.lsp.config('rust_analyzer', { capabilities = capabilities })
+      vim.lsp.config('gopls', { capabilities = capabilities })
+
+      vim.lsp.enable('clangd')
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('pyright')
+      vim.lsp.enable('rust_analyzer')
+      vim.lsp.enable('gopls')
     end,
   }
 }
